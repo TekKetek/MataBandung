1f3d1c334f4bc9bca80bc615d48d9312cfeb6a3d
Haidar Rais <zwingfame@gmail.com>
2018-10-06 21:54:36 +0700

    APIService Done some

--- app/src/main/java/com/pramudana/sam/matabandung/Network/ApiService.java
+++ app/src/main/java/com/pramudana/sam/matabandung/Network/ApiService.java
@@ -1,6 +1,10 @@
 package com.pramudana.sam.matabandung.Network;
 
+import com.pramudana.sam.matabandung.Model.ListEvent.DetailsEvent.ResponseDetailsEvent;
 import com.pramudana.sam.matabandung.Model.ListEvent.ResponseListEvent;
+import com.pramudana.sam.matabandung.Model.Tenant.Data.ResponseDataTenant;
+import com.pramudana.sam.matabandung.Model.Tenant.Detail.ResponseDetail;
+import com.pramudana.sam.matabandung.Model.Tenant.SubSector.ResponseSubSectorTenant;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -13,5 +17,15 @@ import retrofit2.http.POST;
 public interface ApiService {
     @POST("api/v1/public/event/list-event")
     Call<ResponseListEvent> readListEvent();
+    @GET("api/v1/public/tenant/detail-tenant?tenant_id=")
+    Call<ResponseDetailsEvent> detailsEvent();
+    @GET("api/v1/public/tenant/list-subsector")
+    Call<ResponseSubSectorTenant> SubSectorTenant();
+    @POST("api/v1/public/tenant/list-tenant")
+    Call<ResponseDataTenant> dataTenant();
+    @GET("api/v1/public/tenant/detail-tenant?tenant_id=24")
+    Call<ResponseDetail> detailTenant();
+
+
 }
 
