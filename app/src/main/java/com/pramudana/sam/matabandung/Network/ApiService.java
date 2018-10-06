c679753e4a7a813b28994793f2cf27bd184fcb80
Haidar Rais <zwingfame@gmail.com>
2018-10-06 21:16:58 +0700

    Model and Network added

---
+++ app/src/main/java/com/pramudana/sam/matabandung/Network/ApiService.java
@@ -0,0 +1,13 @@
+package com.pramudana.sam.matabandung.Network;
+
+import retrofit2.http.GET;
+
+/**
+ * Created by Hoidar on 10/6/18.
+ */
+
+public interface ApiService {
+//    @GET("everything?q=apple&apiKey=742f9c39ed6c4d5c8874aadb10b34b79")
+//    Call<> readNews();
+}
+
---
+++ app/src/main/java/com/pramudana/sam/matabandung/Network/InstanceRetrofit.java
@@ -0,0 +1,11 @@
+package com.pramudana.sam.matabandung.Network;
+
+/**
+ * Created by Hoidar on 10/6/18.
+ */
+
+public class InstanceRetrofit {
+
+
+
+}
